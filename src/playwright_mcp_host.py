7eeab8d5fb1e24f0bae5488c975716cd99f3f67d
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-27 04:42:29 +0530

    reading email and pass from envs

--- src/playwright_mcp_host.py
+++ src/playwright_mcp_host.py
@@ -1,16 +1,23 @@
 import asyncio
-import json
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 from langgraph.prebuilt import create_react_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
 
-
 from mcp import ClientSession
 from mcp.client.streamable_http import streamablehttp_client
 
 mcp_server_url = "http://localhost:8931/mcp"
+workday_email = os.getenv("WORKDAY_EMAIL")
+workday_pass = os.getenv("WORKDAY_PASS")
+
+prompt = f'''
+open google.com, search for nvidia workday jobs, search for nvidia jobs
+login with my email: {workday_email} with password: {workday_pass}, and apply for a job.
+go to wd5.myworkdayjobs only after searching
+'''
 
 async def main():
     async with streamablehttp_client(f"{mcp_server_url}") as (
@@ -32,7 +39,7 @@ async def main():
             )
 
             agent_response = await agent.ainvoke(
-                {"messages": "open google.com then search for langgraph mcp and summarise the page"}
+                {"messages": prompt},
             )
 
             for m in agent_response["messages"]:
