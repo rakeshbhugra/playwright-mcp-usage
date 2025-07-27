f0bd92f18fac8c2549e9fe7926042ab646fb889b
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-28 02:35:55 +0530

    working reasoning node checkpoint

--- src/agent_graph/nodes/reasoning_node.py
+++ src/agent_graph/nodes/reasoning_node.py
@@ -1,9 +1,37 @@
 from ..state import State
-from utils.llm.litellm_calls import LLMCall
-from utils.llm.llm_models import llm_models
+from src.utils.llm.litellm_calls import LLMCall
+from src.utils.llm.llm_models import llm_models
+from litellm import experimental_mcp_client
+import copy
+import json
+from .node_names import NodeNames
+
+prompt = f'''
+You are a helpful assistant that can answer questions and help with tasks. You can control chrome browser using playwright tools.
+'''
 
 async def reasoning_node(state: State) -> State:
-    # This node is responsible for reasoning and planning.
-    llm = LLMCall(model=llm_models.OpenAIModels.gpt_4_1_mini, just_text=True)
-    
+
+    # writer = state.writer()
+    llm = LLMCall(model=llm_models.OpenAIModels.gpt_4_1_mini, just_text=False)
+    tools = await experimental_mcp_client.load_mcp_tools(session=state.session, format="openai")
+
+    messages_copy = copy.deepcopy(state.messages)
+    messages_copy[0]['content'] = prompt 
+
+    llm_response = await llm.completion_call(
+        messages=messages_copy, 
+        tools=tools, 
+    )
+
+    print("llm_response:", json.dumps(llm_response, indent=2, default=str))
+
+    tool_calls = llm_response["choices"][0]["message"].get("tool_calls")
+    if tool_calls and len(tool_calls) > 0:
+        state.openai_tool = tool_calls[0]
+        state.next_node = NodeNames.TOOL_USE_NODE
+    else:
+        state.openai_tool = None
+        state.next_node = NodeNames.END
+        
     return state
