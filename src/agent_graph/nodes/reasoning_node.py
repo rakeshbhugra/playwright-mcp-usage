bca66c830ee28e011ccddc4f6b4bed9e8da88326
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-28 02:58:16 +0530

    handling both tool and non tool response

--- src/agent_graph/nodes/reasoning_node.py
+++ src/agent_graph/nodes/reasoning_node.py
@@ -5,6 +5,7 @@ from litellm import experimental_mcp_client
 import copy
 import json
 from .node_names import NodeNames
+from src.utils.llm.message_helper import AIMessage
 
 prompt = f'''
 You are a helpful assistant that can answer questions and help with tasks. You can control chrome browser using playwright tools.
@@ -24,7 +25,13 @@ async def reasoning_node(state: State) -> State:
         tools=tools, 
     )
 
-    print("llm_response:", json.dumps(llm_response, indent=2, default=str))
+    # print("llm_response:", json.dumps(llm_response, indent=2, default=str))
+
+    content = llm_response["choices"][0]["message"]["content"]
+    if content:
+        state.messages.append(AIMessage(content=content))
+    else:
+        state.messages.append(llm_response["choices"][0]["message"])
 
     tool_calls = llm_response["choices"][0]["message"].get("tool_calls")
     if tool_calls and len(tool_calls) > 0:
@@ -33,5 +40,7 @@ async def reasoning_node(state: State) -> State:
     else:
         state.openai_tool = None
         state.next_node = NodeNames.END
+        
+        print('\n\nFinal response from LLM:', state.messages[-1]['content'])
         
     return state
