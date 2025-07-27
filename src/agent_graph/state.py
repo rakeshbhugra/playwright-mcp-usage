9861868048b90c037bf0be3f21b05c245dab6946
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-28 02:38:10 +0530

    added next_node, openai_tool, writer, mcp session

--- src/agent_graph/state.py
+++ src/agent_graph/state.py
@@ -1,4 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional, Any
+from langgraph.config import StreamWriter
+from mcp import ClientSession
 
 class State(BaseModel):
     messages: list[dict]
+    session: Optional[ClientSession] = None
+    writer: Optional[StreamWriter] = None
+    openai_tool: Optional[Any] = None
+    next_node: Optional[str] = None
+
+    class Config:
+        arbitrary_types_allowed = True
