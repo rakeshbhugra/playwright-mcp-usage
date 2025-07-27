23efc281ba52593f1df12a5b1e09fac62b4bd303
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-28 03:30:32 +0530

    added function to read resume and create prompt based on resume

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 from src.agent_graph.agent import PlaywrightAgent
 import asyncio
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-workday_email = os.getenv("WORKDAY_EMAIL")
-workday_pass = os.getenv("WORKDAY_PASS")
-
-prompt = f'''
-open google.com, search for nvidia workday jobs, search for nvidia jobs
-login with my email: {workday_email} with password: {workday_pass}, and apply for a job.
-go to wd5.myworkdayjobs only after searching
-'''
+from src.utils.read_resume import ResumeReader
+from src.utils.get_prompt_with_resume_details import get_prompt
 
 async def main():
     agent = PlaywrightAgent()
     history_messages = [
         {"role": "system", "content": "You are a helpful assistant."},
     ]
-    user_message = prompt
+
+    resume_path = "data_files/resumes/Lin Mei_Experiened Level Software.pdf"
+    resume_text = ResumeReader(resume_path).read_resume("pymupdf")
+
+    user_message = get_prompt(resume_text)
 
     await agent.run(history_messages, user_message)
 
