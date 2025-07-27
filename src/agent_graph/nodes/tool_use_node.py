b8ffb1e445a03604d8d7298cc59e4c51840bad4b
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-28 02:36:04 +0530

    wip: tool use node

--- src/agent_graph/nodes/tool_use_node.py
+++ src/agent_graph/nodes/tool_use_node.py
@@ -1,5 +1,14 @@
 from ..state import State
+from litellm import experimental_mcp_client
 
 async def tool_use_node(state: State) -> State:
+
     # This node is responsible for using tools.
+    call_result = await experimental_mcp_client.call_openai_tool(
+        session=state.session,
+        openai_tool=state.openai_tool
+    )
+
+    print("MCP Tool Call Result:", call_result)
+
     return state
