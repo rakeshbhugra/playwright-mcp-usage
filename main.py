c21999e036f1a7224ba7dad0eb9a6f47de696168
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-07-28 03:09:20 +0530

    created main file calling agent

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from playwright-mcp-usage!")
+from src.agent_graph.agent import PlaywrightAgent
+import asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+workday_email = os.getenv("WORKDAY_EMAIL")
+workday_pass = os.getenv("WORKDAY_PASS")
+
+prompt = f'''
+open google.com, search for nvidia workday jobs, search for nvidia jobs
+login with my email: {workday_email} with password: {workday_pass}, and apply for a job.
+go to wd5.myworkdayjobs only after searching
+'''
+
+async def main():
+    agent = PlaywrightAgent()
+    history_messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+    ]
+    user_message = prompt
+
+    await agent.run(history_messages, user_message)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
